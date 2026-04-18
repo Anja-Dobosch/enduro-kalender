94a4d291a5ccf0bf74ba6f96c5e448b1fbea8cd1
Anja-Dobosch <dobosch72@gmail.com>
2026-04-18 18:07:23 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+// Cache deaktiviert - immer frische Version laden
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request));
+});
